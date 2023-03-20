276a01307b54f208da8ca733aa1be8f0aa5f7f8c
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2023-03-20 13:53:21 -0400

    update

---
+++ analysis/freeze2/testing/write_contrast_jobs.R
@@ -0,0 +1,59 @@
+# Gabriel Hoffman
+# Write LSF job files to run dreamlet on contrasts
+
+# Load CONTRASTS and metadata
+load("/sc/arion/projects/psychAD/NPS-AD/freeze2_rc/analysis/contrasts_for_dreamlet.Rdata")
+
+write_job = function( variable_type, ctst_key ){
+
+	outpath = paste0("/sc/arion/projects/psychAD/NPS-AD/freeze2_rc/analysis/results/", ctst_key)
+
+	outfile = paste0(outpath, "/", ctst_key)
+
+	if( ! dir.exists(outpath) ) dir.create(outpath)
+
+	SRC = "/sc/arion/projects/CommonMind/hoffman/NPS-AD/work/nps_ad/analysis/freeze2/testing/run_dreamlet_contrasts.Rmd"
+
+	dataset = gsub("([a-zA-Z]+).*", "\\1", ctst_key)
+
+	cmd = paste0("rmarkdown::render('", SRC, "', 
+					output_file = '", outfile, "', 
+					params = list(dataset = '", dataset, "',
+					variable_type = '", variable_type, "', 
+					ctst_key = '", ctst_key, "'))")
+
+	logs = paste0("/sc/arion/projects/psychAD/NPS-AD/freeze2_rc/analysis/logs/", ctst_key, "_")
+
+	txt = paste0("#!/bin/bash
+	#BSUB -J ", ctst_key, "
+	#BSUB -P acc_CommonMind
+	#BSUB -q premium
+	#BSUB -n 6
+	#BSUB -R rusage[mem=12000]
+	#BSUB -R span[hosts=1]
+	#BSUB -W 48:00
+	#BSUB -o ", logs, "%J.stdout
+	#BSUB -eo ", logs, "%J.stderr
+	#BSUB -L /bin/bash
+
+	ml hdf5/1.12.1 libpng/16 R/4.2.0 pandoc/2.6
+
+	R_LIBS_USER=/hpc/users/hoffmg01/.Rlib/R_420
+	R_LIBS=${R_LIBS_USER}:/hpc/users/hoffmg01/.Rlib/R_420:/hpc/packages/minerva-centos7/rpackages/4.2.0/site-library:/hpc/packages/minerva-centos7/rpackages/bioconductor/3.15
+
+	Rscript -e \"", cmd, "\"
+
+	")
+
+	jobFile = paste0("/sc/arion/projects/psychAD/NPS-AD/freeze2_rc/analysis/jobs/", ctst_key, ".lsf") 
+
+	write(txt, jobFile)
+}
+
+
+for( variable_type in names(CONTRASTS)){
+	for( ctst_key in names(CONTRASTS[[variable_type]]) ){
+
+		write_job( variable_type, ctst_key)
+	}
+}
