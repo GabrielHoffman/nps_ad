7ada8308d5fd4b039e029129fc800e32536e1b41
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2023-03-20 14:54:58 -0400

    update

--- analysis/freeze2/testing/write_contrast_jobs.R
+++ analysis/freeze2/testing/write_contrast_jobs.R
@@ -25,25 +25,25 @@ write_job = function( variable_type, ctst_key, dataset ){
 	logs = paste0("/sc/arion/projects/psychAD/NPS-AD/freeze2_rc/analysis/logs/", suffix, "_")
 
 	txt = paste0("#!/bin/bash
-	#BSUB -J ", suffix, "
-	#BSUB -P acc_CommonMind
-	#BSUB -q premium
-	#BSUB -n 6
-	#BSUB -R rusage[mem=12000]
-	#BSUB -R span[hosts=1]
-	#BSUB -W 48:00
-	#BSUB -o ", logs, "%J.stdout
-	#BSUB -eo ", logs, "%J.stderr
-	#BSUB -L /bin/bash
+#BSUB -J ", suffix, "
+#BSUB -P acc_CommonMind
+#BSUB -q premium
+#BSUB -n 6
+#BSUB -R rusage[mem=12000]
+#BSUB -R span[hosts=1]
+#BSUB -W 48:00
+#BSUB -o ", logs, "%J.stdout
+#BSUB -eo ", logs, "%J.stderr
+#BSUB -L /bin/bash
 
-	ml hdf5/1.12.1 libpng/16 R/4.2.0 pandoc/2.6
+ml hdf5/1.12.1 libpng/16 R/4.2.0 pandoc/2.6
 
-	R_LIBS_USER=/hpc/users/hoffmg01/.Rlib/R_420
-	R_LIBS=${R_LIBS_USER}:/hpc/users/hoffmg01/.Rlib/R_420:/hpc/packages/minerva-centos7/rpackages/4.2.0/site-library:/hpc/packages/minerva-centos7/rpackages/bioconductor/3.15
+R_LIBS_USER=/hpc/users/hoffmg01/.Rlib/R_420
+R_LIBS=${R_LIBS_USER}:/hpc/users/hoffmg01/.Rlib/R_420:/hpc/packages/minerva-centos7/rpackages/4.2.0/site-library:/hpc/packages/minerva-centos7/rpackages/bioconductor/3.15
 
-	Rscript -e \"", cmd, "\"
+Rscript -e \"", cmd, "\"
 
-	")
+")
 
 	jobFile = paste0("/sc/arion/projects/psychAD/NPS-AD/freeze2_rc/analysis/jobs/", suffix, ".lsf") 
 
