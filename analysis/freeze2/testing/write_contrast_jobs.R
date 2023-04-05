bf1b1319742854c25d45a20a3678190c4fd59f3a
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2023-04-05 11:10:33 -0400

    update

--- analysis/freeze2/testing/write_contrast_jobs.R
+++ analysis/freeze2/testing/write_contrast_jobs.R
@@ -20,6 +20,7 @@ write_job = function( variable_type, ctst_key, dataset, method, SampleLevel, Ann
 
 	cmd = paste0("rmarkdown::render('", SRC, "', 
 output_file = '", outfile, "', 
+output_file = '", intermediates_dir, "', 
 params = list(dataset = '", dataset, "',
 variable_type = '", variable_type, "', 
 SampleLevel = '", SampleLevel, "', 
