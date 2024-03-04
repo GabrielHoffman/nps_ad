6720d0160d48ca66e3c344ed2aa8475632e92681
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2024-03-03 20:49:46 -0500

    add mashr

--- analysis/freeze2/testing/meta_analysis.R
+++ analysis/freeze2/testing/meta_analysis.R
@@ -48,12 +48,84 @@ df2 = df %>%
 	filter(coef %in% coefMatch) 
 
 cat("Analysis...\n")
-res = df2 %>%
+res.meta = df2 %>%
+		arrange(ID) %>% slice_head(n = 20046) %>%
 		meta_analysis(method = opt$method, group=grp) %>%
 		mutate( coef = opt$code, Trait = df_meta$Contrast.desc[i])
 
 # write to file
 cat("Writing parquet...\n")
 outfile = paste0(opt$outFolder, "/topTable_meta_", opt$code, "_", opt$method,".parquet")
-write_parquet(res, outfile )
-	
+write_parquet(res.meta, outfile )
+
+# mashr 
+#######
+
+library(mashr)
+
+run_mash_local = function(res, annLvl){
+
+	res = res %>% 
+		filter(AnnoLevel == annLvl)
+
+	lvls = unique(res$assay)
+	res$assay <- factor(res$assay, lvls)
+
+	# convert to matricies
+	B <- dreamlet:::tabToMatrix(res, "estimate")
+	S <- dreamlet:::tabToMatrix(res, "std.error")
+
+	# only keep columns with variance in logFC
+	cv <- colVars(B, na.rm = TRUE, useNames = TRUE)
+	keep <- (cv > 0) & !is.na(cv)
+	B <- B[, keep, drop = FALSE]
+	S <- S[, keep, drop = FALSE]
+
+	# run mashr on these matricies
+	#-----------------------------
+
+	# set up
+	# NA's are replaced with beta = 0 with se = 1e6
+	data <- mash_set_data(B, S)
+
+	# estimate model parameters
+	U.c <- cov_canonical(data)
+
+	# Estimate correlation structure
+	V.em <- mash_estimate_corr_em(data, U.c, details = TRUE)
+
+	# copy model to drop NA terms
+	model <- V.em$mash.model
+
+	# B has the same ordering as these, so replace corresponding elements with NA
+	# this revents non-sensiccal results for coefficients that were originally NA
+	idx <- which(is.na(B))
+	model$result$PosteriorMean[idx] <- NA
+	model$result$PosteriorSD[idx] <- NA
+	model$result$NegativeProb[idx] <- NA
+	model$result$lfsr[idx] <- NA
+
+	# format results as new object
+	res_mash = new("dreamlet_mash_result", list(model = model, logFC.original = B, coefList = opt$code))
+
+	file = paste0(opt$outFolder, "/res_mash_", annLvl, "_", opt$code, '_', opt$method, ".RDS")
+	saveRDS(res_mash, file=file)
+
+	ncol(B)
+}
+
+lvls = setdiff(unique(res.meta$AnnoLevel), "bulk")
+
+lapply(lvls, function(x){
+	message(x)
+	run_mash_local(res.meta, x)
+	})
+
+
+
+
+
+
+
+
+
